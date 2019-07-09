21ba2321c860995e40efc341ca9d1e5c3add716c
CongLiu-CN <upcliucong@gmail.com>
2019-07-09 17:25:13 +0800

    Add BrowserRouter to wrap App and add Route inside App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPageDetail = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>{`HatsPageDetail${props.match.params.id}`}</h1>
+    </div>
+  );
+};
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage Main</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route path="/hats/:id" component={HatsPageDetail} />
+      </Switch>
     </div>
   );
 }
