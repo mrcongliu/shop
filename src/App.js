6e178d4ac29547ac034fa9809674fc9e2febef31
CongLiu-CN <upcliucong@gmail.com>
2019-07-11 22:12:50 +0800

    add shop page

--- src/App.js
+++ src/App.js
@@ -4,29 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPageDetail = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>{`HatsPageDetail${props.match.params.id}`}</h1>
-    </div>
-  );
-};
-
-const HatsPage = () => (
-  <div>
-    <h1>HatsPage Main</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
-        <Route path="/hats/:id" component={HatsPageDetail} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
