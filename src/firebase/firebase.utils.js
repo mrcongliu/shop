145fbdc0630778db56781482cedee0e19c950e0d
CongLiu-CN <upcliucong@gmail.com>
2019-07-16 21:27:13 +0800

    add user data to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,35 @@ const config = {
   appId: "1:304724189902:web:6772511a116f66be"
 };
 
+/* send a query to firebase */
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  /* check if there is a user in that database, if there isn't, create a user using the data from userAuth object. */
+  if (!snapShot.exits) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  /* In case we want to use it. */
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
